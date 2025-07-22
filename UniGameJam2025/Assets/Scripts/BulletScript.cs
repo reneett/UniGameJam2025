5b2afb48cdedb28e1537ff36f3dcb31df9cf750e
ljcui1 <yangyangjiaocui@gmail.com>
2025-07-22 14:57:00 -0700

    fixed issues with bullet rotation and destorying on invisible

--- UniGameJam2025/Assets/Scripts/BulletScript.cs
+++ UniGameJam2025/Assets/Scripts/BulletScript.cs
@@ -2,54 +2,34 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    [SerializeField] private GameObject projectilePrefab;
-    [SerializeField] private Transform firePoint;
-    private float projectileSpeed = 10f;
-
-    //camera
-    [SerializeField] private Camera mainCam;
-    //clamp rotation
-    private float minRotation = -90f;
-    private float maxRotation = 90f;
-
     //bullet lifetime
-    [SerializeField] private float lifeTime = 3f;
+    //[SerializeField] private float lifeTime = 3f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //destroy projectile after time
-        Destroy(gameObject, lifeTime);
+        //Destroy(gameObject, lifeTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //get mouse position in world space
-        Vector3 mouseWorldPos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        mouseWorldPos.z = 0;
-
-        //get direction/angle
-        Vector3 dir = mouseWorldPos - transform.position;
-        //calc angle in degrees
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f; //points up instead of right (-90f)
-
-        //clamp rotation
-        float clampedAngle = Mathf.Clamp(angle, minRotation, maxRotation);
-
-        //apply rotation
-        transform.rotation = Quaternion.Euler(0, 0, clampedAngle);
-
-        //mouseclick shooting
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 direction = (mouseWorldPos - firePoint.position).normalized;
-
-            GameObject projectile = Instantiate(projectilePrefab, firePoint.position, Quaternion.identity);
-            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-            rb.freezeRotation = true;
-            rb.linearVelocity = direction * projectileSpeed;
-
-        }
+       
     }
+
+    /*private void OnDestroy()
+    {
+        // Inform tower to allow another shot
+        PlayerScript player = FindObjectOfType<PlayerScript>();
+        if (player != null)
+        {
+            player.OnBulletDestroyed();
+        }
+    }*/
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
 }
