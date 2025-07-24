5a82beb963bcfe96846f0c4d9aa7af774fa7459c
ljcui1 <yangyangjiaocui@gmail.com>
2025-07-24 15:58:11 -0700

    added pause and unpause

--- UniGameJam2025/Assets/Scripts/UIManager.cs
+++ UniGameJam2025/Assets/Scripts/UIManager.cs
@@ -9,12 +9,32 @@ public class UIManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pause.gameObject.SetActive(true);
+        resume.gameObject.SetActive(false);
+        pauseMenu.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void clickPause()
+    {
+        Time.timeScale = 0f;
+
+        pause.gameObject.SetActive(false);
+        resume.gameObject.SetActive(true);
+        pauseMenu.gameObject.SetActive(true);
+    }
+
+    public void clickResume()
+    {
+        Time.timeScale = 1f;
+
+        pause.gameObject.SetActive(true);
+        resume.gameObject.SetActive(false);
+        pauseMenu.gameObject.SetActive(false);
     }
 }
