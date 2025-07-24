93d7bff35709c1f86d37315d5b2b35936e1feff3
ljcui1 <yangyangjiaocui@gmail.com>
2025-07-24 15:54:44 -0700

    pause ui set up

--- UniGameJam2025/Assets/Scripts/UIManager.cs
+++ UniGameJam2025/Assets/Scripts/UIManager.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private Button pause;
+    [SerializeField] private Button resume;
+    [SerializeField] private Image pauseMenu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
