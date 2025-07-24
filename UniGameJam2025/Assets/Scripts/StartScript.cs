7877969392acd9c0c3d32551ddf8187e7d32d535
ljcui1 <yangyangjiaocui@gmail.com>
2025-07-24 15:33:41 -0700

    start scene made

---
+++ UniGameJam2025/Assets/Scripts/StartScript.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class StartScript : MonoBehaviour
+{
+    [SerializeField] private Button play;
+    [SerializeField] private Button credits;
+    [SerializeField] private Button back;
+    [SerializeField] private Image background;
+
+    [SerializeField] private TextMeshProUGUI creditText;
+    [SerializeField] private TextMeshProUGUI title;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //play.enabled = true;
+        //credits.enabled = true;
+        //background.enabled = true;
+        //title.enabled = true;
+        play.gameObject.SetActive(true);
+        credits.gameObject.SetActive(true);
+        background.gameObject.SetActive(true);
+        title.gameObject.SetActive(true);
+
+        //back.enabled = false;
+        //creditText.enabled = false;
+        back.gameObject.SetActive(false);
+        creditText.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void clickCredits()
+    {
+        play.gameObject.SetActive(false);
+        credits.gameObject.SetActive(false);
+        title.gameObject.SetActive(false);
+
+        back.gameObject.SetActive(true);
+        creditText.gameObject.SetActive(true);
+    }
+
+    public void clickBack()
+    {
+        play.gameObject.SetActive(true);
+        credits.gameObject.SetActive(true);
+        title.gameObject.SetActive(true);
+
+        back.gameObject.SetActive(false);
+        creditText.gameObject.SetActive(false);
+    }
+}
